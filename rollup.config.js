59814a7a58273326afec8d9649c2d2a10098c453
Charlie Briggs <charlie.briggs@bbc.co.uk>
2017-09-22 11:40:05 +0100

    Fix axios import and use commonjs rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 
 export default {
@@ -16,6 +17,7 @@ export default {
     postcss({
       extensions: [ '.css' ],
     }),
+    commonjs(),
     resolve({
       main: true,
       browser: true
--- statics/hack.js
+++ statics/hack.js
@@ -1,5 +1,5 @@
 import {h, render} from 'preact';
-import axois from 'axois';
+import axios from 'axios/dist/axios';
 import './hack.css'
 
 const contentId = document.getElementById('site-content').getAttribute('data-content-id');
@@ -51,20 +51,13 @@ const stubData = [
 	        "Healthcare&LifeSciences"
 	    ],
 	    "title": "FT Digital Health Summit Europe",
-		"imageSrc": 'https://live.ft.com/var/ftlive/storage/images/events/2017/ft-digital-health-summit-usa/816896-4-eng-GB/FT-Digital-Health-Summit-USA_eventcardimage.png'
+		"imageSrc": 'https://live.ft.com/var/ftlive/storage/images/events/2017/ft-digital-health-summit-usa/816896-4-eng-GB/FT-Digital-Health-Summit-USA_eventcardimage.png',
 	    "location": "London",
 	    "date": "2018-06-19T00:00:00.000+01:00",
 	    "speakers": [],
 	    "description": "Assess how patients are being empowered and further involved in the transformation of healthcareHear how collaborations between pharma and big tech are enriching the commercial viability of digital healthMeet and greet with the full array of industry stakeholders, from healthcare to technology, pharma to start-ups, regulation to governmentIdentify solutions in overcoming cyber security threats in digital healthDon't get left behind - be part of a must-attend event mapping the continued transformation of healthcare\n    \n        FT Live has a reputation for delivering very senior board level audiences to attend a wide range of world class thought-leadership events across the globe for nearly 40 years.The FT Digital Health Summit Europe will be attended by:Life science companiesPharmaceutical and biotechnology companies Healthcare providers Multi-technology corporations Investment banks Medical device manufacturers Health insurers Health/big data companies Venture capitalists Health regulatory agencies Mobile app providers Mobile operators and service providers Technology consultancies Management consultancies Telemedicine companies Mobile technology manufacturers Digital health companies Data integrators/analytics/brokers Genomics/personalised medicine related companies Wireless-device manufacturers\n    \n        Gain access to senior decision makers across the full spectrum of the healthcare industryBrand alignment with the FT and its unrivalled credibilityDemonstrate thought leadership around the current critical issues affecting the healthcare sectorBenefit from sustained international visibility through our advertising and marketing campaigns\n    \n        Ticket Type                                                                                FeeStandard Summit Ticket£899 +VAT"
 	}
 ];
-
-let data;
-try {
-	data = await axios.get(`https://localhost:8080/get-event/${contentId}`);
-} catch (error) {
-	data = stubData;
-}
 
 const dataMapper = ({title, location, uri, imageSrc}) => ({
 	location,
@@ -73,14 +66,20 @@ const dataMapper = ({title, location, uri, imageSrc}) => ({
 	url: uri
 });
 
-render((
-   	<TeaserCollection 
-   		heading={<TeaserHeading text="Financial Times Live Events" url="https://live.ft.com" />}
-   	>
-	   {stubData.map((data) => (
-	   		<FTLiveItem 
-	   			{...dataMapper(data)}
-	   		/>
-	   ))}
-	</TeaserCollection>
-), document.getElementById('hack-root'));
+
+axios.get(`https://localhost:8080/get-event/${contentId}`)
+.catch((error) => {
+	console.error('Hack call failed', error);
+}).then((data) => {
+	render((
+	   	<TeaserCollection 
+	   		heading={<TeaserHeading text="Financial Times Live Events" url="https://live.ft.com" />}
+	   	>
+		   {stubData.map((data) => (
+		   		<FTLiveItem 
+		   			{...dataMapper(data)}
+		   		/>
+		   ))}
+		</TeaserCollection>
+	), document.getElementById('hack-root'));
+});
